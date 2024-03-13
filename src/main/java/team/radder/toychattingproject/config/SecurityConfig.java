a97a6211c1764957ca589ab790170b49082c337d
기묘한 주니어 개발자 <alcuz137@gmail.com>
2024-03-14 01:29:43 +0900

    [Feat : 스프링 시큐리티 API 추가]
    로그인 관련 시큐리티 추가

---
+++ src/main/java/team/radder/toychattingproject/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package team.radder.toychattingproject.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.formLogin(config -> config.loginPage("/login"));
+
+        http.csrf(config -> config.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest
+                .toStaticResources().atCommonLocations());
+    }
+}
--- src/main/java/team/radder/toychattingproject/controller/LoginController.java
+++ src/main/java/team/radder/toychattingproject/controller/LoginController.java
@@ -1,11 +1,11 @@
-package team.radder.toychattingproject;
+package team.radder.toychattingproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping("/signin")
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
