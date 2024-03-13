423de64aed652b072f83989d2bc977962a48c998
Eunji1217 <unji1361@gmail.com>
2024-03-13 19:04:28 +0900

    [Feat: project setting]

---
+++ src/main/java/team/radder/toychattingproject/ToyChattingProjectApplication.java
@@ -0,0 +1,13 @@
+package team.radder.toychattingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyChattingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyChattingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/radder/toychattingproject/ToyChattingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package team.radder.toychattingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyChattingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
